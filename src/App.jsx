6b5264da9b3cc448a02bd3d8c0dd6921679ea9fa
EmirCtis0 <emirinaner@gmail.com>
2025-08-17 14:36:56 +0300

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-blue-400">
+        Tailwind v4 Çalışıyor 🎉
+      </h1>
+    </div>
+  )
+}
+
+export default App
